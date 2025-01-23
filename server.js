c87f02ba30263419f136e744ddfa4221b3f812ca
Eman-Qadry <wafaakadry756@gmail.com>
2025-01-23 15:19:03 +0200

    allow options in cors

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+app.options('*', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.sendStatus(200);
+  });
+  
 app.use('/admin/auth',authRouter);
 app.use('/admin/topics',topicRouter);
 app.use('/admin/videos',vidrouter);
