a0ccb544fa55411a9e63d01acab3bf0a5b44c62b
Eman-Qadry <wafaakadry756@gmail.com>
2025-01-23 13:48:53 +0200

    edit database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const connectDB=async ()=>{
     try{
-        mongoose.connect('mongodb://localhost:27017/coursesAPI',{
+      await  mongoose.connect(process.env.DATABASE_URL,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
          
